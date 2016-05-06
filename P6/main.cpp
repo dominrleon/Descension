4365738b3c2432e789c2cccc3042050512a9d383
dominrleon <domingorodriguezle@icloud.com>
2016-05-06 18:52:28 +0200

    Initial Commit

---
+++ P6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P6
+//
+//  Created by Domingo Rodríguez León on 6/5/16.
+//  Copyright © 2016 Domingo Rodríguez León. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
